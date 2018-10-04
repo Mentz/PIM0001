4edd004d99bf7cf1b288c50e841cd2dbe434cb5e
Lucas L. Mentz <lucaslittermentz@gmail.com>
2018-10-04 11:54:27 -0300

    Feito F

--- Exercicios/T3-AlargamentoContraste/f/equaliza.py
+++ Exercicios/T3-AlargamentoContraste/f/equaliza.py
@@ -6,26 +6,17 @@ import numpy as np
 import cv2
 from sys import exit,argv
 
-def remap(c, omin, omax, nmin, nmax):
-	nc = (c - omin) * (nmax - nmin) / (omax - omin) + nmin
-	return nc
+def BGR_to_YUV(img):
+	new_img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+	return new_img
 
-def clamp(v, vmin, vmax):
-	if v < vmin:
-		return vmin
-	if v > vmax:
-		return vmax
-	return v
-
-def diff(a, b):
-	if a >= b:
-		return a - b
-	if a < b:
-		return b - a
+def YUV_to_BGR(img):
+	new_img = cv2.cvtColor(img, cv2.COLOR_YUV2BGR)
+	return new_img
 
 def equalizarContraste(img):
-	x, y = len(img), len(img[0])
-	new_img = np.zeros((x, y, 3), np.uint8)
+	y, x = len(img), len(img[0])
+	new_img = img.copy()
 	# MN é a área da imagem ou quantidade de pixels (x*y)
 	MN = float(x*y)
 	# L é a profundidade de cor (cinza)
@@ -41,18 +32,19 @@ def equalizarContraste(img):
 	# gerar o valor de saida (sk)
 	s = [0]*L
 	g1 = (L - 1)/MN
-	for k in xrange(0, 256):
+	for k in range(0, 256):
 		g2 = 0
 		for j in range(0, k+1):
 			g2 += hist[j]
 		s[k] = int(round(g1 * g2))
-	
+
 	print(s)
 
 	# reaplicar os valores para cada pixel usando s[k]
-	for j in xrange(y):
-		for i in xrange(x):
-			new_img[i, j] = [s[img[i, j]]]*3
+	for i in range(y):
+		for j in range(x):
+			# nova_img    = (equalizacao de y)
+			new_img[i, j] = s[img[i, j]]
 
 	return new_img
 
@@ -61,7 +53,7 @@ if __name__ == '__main__':
 	x, y, l = 10, 10, 0
 	path = 'garbage'
 	outpath = 'newGarbage'
-	
+
 
 	''' Ler variaveis da linha de comando '''
 	if len(argv) >= 3:
@@ -74,7 +66,11 @@ if __name__ == '__main__':
 			  % argv[0])
 		exit(1)
 
-	img = cv2.imread(path, 0)
-	nimg = equalizarContraste(img)
+	img = cv2.imread(path)
+	img_yuv = BGR_to_YUV(img)
+	img_y, img_u, img_v = cv2.split(img_yuv)
+	nimg_y = equalizarContraste(img_y)
+	nimg_yuv = cv2.merge((nimg_y, img_u, img_v))
+	nimg = YUV_to_BGR(nimg_yuv)
 
 	cv2.imwrite(outpath, nimg)
--- Exercicios/T3-AlargamentoContraste/f/hist.py
+++ Exercicios/T3-AlargamentoContraste/f/hist.py
@@ -3,7 +3,18 @@ import cv2
 from matplotlib import pyplot
 import sys
 
-img = cv2.imread(sys.argv[1], 0)
-fig = pyplot.hist(img.ravel(), 256, [0,256]);
-pyplot.xlim([0,256])
+img = cv2.imread(sys.argv[1])
+
+# Para histograma em cinza
+# fig = pyplot.hist(img.ravel(), 256, [0,256], histtype='step')
+# pyplot.xlim([0,256])
+
+# Para histograma RGB
+color = ('b','g','r')
+pyplot.figure()
+for i,col in enumerate(color):
+    histr = cv2.calcHist([img],[i],None,[256],[0,256])
+    pyplot.plot(histr,color = col)
+    pyplot.xlim([0,256])
+# pyplot.show()
 pyplot.savefig(sys.argv[2])
