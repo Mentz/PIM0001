e6c65ab777542b37846faf470d38d85eec5cd147
Lucas L. Mentz <lucaslittermentz@gmail.com>
2018-11-21 00:12:54 -0200

    Implementado parte morfológica, falta contagem

--- Exercicios/Moedas/morfo.py
+++ Exercicios/Moedas/morfo.py
@@ -11,15 +11,8 @@ import sys
 import numpy as np
 import cv2
 
-
 def limiarizar(img):
 	limiar = skf.threshold_otsu(img)
-	# y, x = len(img), len(img[0])
-	# out = np.zeros((y, x, 3), np.uint8)
-	# for i in range(y):
-	# 	for j in range(x):
-	# 		out[i][j] = (0, 0, 0) if img[i][j] > limiar else (255,255,255)
-	# return out
 	out = img < limiar
 	return out
 
@@ -30,13 +23,6 @@ def toBGR(img):
 		for j in range(x):
 			out[i][j] = (0,0,0) if img[i][j] == 0 else (255,255,255)
 	return out
-
-# NÃO PRECISA IMPLEMENTAR DILATAÇÃO E EROSÃO
-# O objetivo é usar abertura e fechamento para contagem
-# de moedas em uma foto que está no seu celular, Mentz!
-# Biblioteca:
-# pymorph ou skimage
-# Usar limiarização de OTSU
 
 def dilatacao(img, est):
 	out = skm.binary_dilation(img, est)
@@ -56,28 +42,21 @@ def fechamento(img, est):
 	out = erosao(img2, est)
 	return out
 
-if __name__ == "__main__":
-	# if len(sys.argv) <= 4:
-		# print("argumentos: <imagem> <estruturante> <limiar[0,255]> <saida>")
-	# if len(sys.argv) <= 3:
-	# 	print("argumentos: <imagem> <estruturante> <saida>")
-	# 	sys.exit(1)
-	
-	# limiar = int(sys.argv[3])
-	# saida  = sys.argv[4]
-	# saida  = sys.argv[3]
+# Essa função faz a contagem do número de elementos contíguos (blobs)
+# existentes numa imagem binária e retorna esse valor.
+def contaElementos(img):
+	return 8
 
+if __name__ == "__main__":
 	# Carregar figura em escala de cinza (facilita limiarização)
+	print("Calculando limiarização da imagem")
 	impath = "img/moedas.jpg"
 	img = cv2.imread(impath, 0)
-	# est = cv2.imread(espath, 0)
-
-	# cv2.imshow("argh", est)
-	# cv2.waitKey()
-	# cv2.destroyAllWindows()
-
 	img = limiarizar(img)
 	img = fechamento(img, skm.disk(4))
+
+	# Esse comando é para preencher buracos internos causados pelo
+	# alto brilho em algumas das moedas:
 	img = skm.remove_small_holes(img, area_threshold=768)
 
 	cv2.imwrite("etapas/moedas_limiar.png", toBGR(img))
@@ -86,34 +65,52 @@ if __name__ == "__main__":
 	# cv2.waitKey()
 	# cv2.destroyAllWindows()
 	
+	# Vetor que guardará cada versão filtrada da imagem original
+	imff = list()
+
+	# A ordem de tamanho decrescente das moedas é a seguinte:
+	# 100
+	# 25-2 (marrom)
+	# 25-1 (prata)
+	# 50
+	# 10-2 (prata)    igual ao 5-2 em tamanho
+	# 5-2 (marrom)    igual ao 10-2 em tamanho
+	# 5-1 (prata)
+	# 10-1 (dourado)
+
 	# Cada elemento estruturante será carregado de maneira similar
-	for tipo in ["5-1", "5-2", "10-1", "10-2", "25-1", "25-2", "50", "100"]:
+	moedas = ["100", "25-2", "25-1", "50", "10-2", "5-2", "5-1", "10-1"]
+	for tipo in moedas:
+		print("Calculando estruturante da moeda {}".format(tipo))
 		espath = "img/m" + tipo + ".png"
 		est = cv2.imread(espath, 0)
 		est = limiarizar(est)
-		est = fechamento(est, skm.disk(4))
-		est = skm.remove_small_holes(est, area_threshold=256)
+		est = fechamento(est, skm.disk(3))
+
+		# Esse comando é para preencher buracos internos causados pelo
+		# alto brilho em algumas das moedas:
+		est = skm.remove_small_holes(est, area_threshold=768)
+
+		# Erosão para reduzir ligeiramente o diâmetro da moeda
+		# estruturante, para caso existam outras moedas do mesmo
+		# tipo com tamanho ligeiramente diferente na imagem.
+		est = erosao(est, skm.diamond(1))
 
 		cv2.imwrite("etapas/est_m{}.png".format(tipo), toBGR(est))
 
+		# Filtra a imagem por elementos iguais ou maiores que o estruturante
+		print("Filtrando moedas iguais ou maiores que o tipo {}".format(tipo))
 		imget = erosao(img, est)
+		imff.append(imget)
 		cv2.imwrite("etapas/im-m{}.png".format(tipo), toBGR(imget))
 
 		# cv2.imshow("argh", toBGR(est))
 		# cv2.waitKey()
 		# cv2.destroyAllWindows()
 
-	# est = limiarizar(est)
-	# est = fechamento(est, skm.disk(30))
+	# Contagem de moedas em cada etapa
+	contagem = dict()
+	for tipo in moedas:
+		contagem[tipo] = contaElementos(img)
 
-	# op = input("Quer executar:\n [d]ilatação, [e]rosão, [a]bertura ou [f]echamento? ")
-	# if op == 'd':
-	# 	outimg = dilatacao(img, est)
-	# if op == 'e':
-	# 	outimg = erosao(img, est)
-	# if op == 'a':
-	# 	outimg = abertura(img, est)
-	# if op == 'f':
-	# 	outimg = fechamento(img, est)
-
-	# cv2.imwrite(saida, toBGR(outimg))
+	print("Fim! -- Na verdade, falta apresentar o valor das moedas na foto.")
